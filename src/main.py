d7bf5a24ca1f6f905611b766b9a9f166fd5e7aad
Adam Stox <adamstox@protonmail.com>
2023-03-29 23:54:34 -0400

    Getting error code 561211770 when trying to create MOD. But no errors

---
+++ src/main.py
@@ -0,0 +1,26 @@
+# main.py
+import rumps
+from record_and_transcribe import start_recording, stop_recording
+
+class RecorderApp(rumps.App):
+    def __init__(self):
+        super(RecorderApp, self).__init__("Recorder")
+        self.start_button = rumps.MenuItem(title="Start Recording", callback=self.start)
+        self.stop_button = rumps.MenuItem(title="Stop Recording", callback=self.stop)
+        self.menu = [self.start_button, self.stop_button]
+        self.stop_button.set_callback(None)
+
+    @rumps.clicked("Start Recording")
+    def start(self, _):
+        self.start_button.set_callback(None)
+        self.stop_button.set_callback(self.stop)
+        start_recording()
+
+    @rumps.clicked("Stop Recording")
+    def stop(self, _):
+        self.start_button.set_callback(self.start)
+        self.stop_button.set_callback(None)
+        stop_recording()
+
+if __name__ == "__main__":
+    RecorderApp().run()
