4a398115e2c0b63dfb9be105b1d1fd61955a5f54
Adam Stox <adamstox@protonmail.com>
2023-04-16 18:48:53 -0400

    set size of new terminal

--- main.py
+++ main.py
@@ -8,8 +8,14 @@ cwd = os.getcwd()
 path = 'src/main.py'
 
 # Define the AppleScript command to open a new Terminal window
-applescript_command = f'tell application "Terminal" to do script "python3 {os.path.join(cwd, path)}"'
+applescript_command = f'''
+tell application "Terminal"
+    set newWindow to do script "python3 {os.path.join(cwd, path)}"
+    set number of columns of newWindow to 200
+    set number of rows of newWindow to 60
+    activate
+end tell
+'''
 
 # Open a new Terminal window and run the command
 subprocess.Popen(['osascript', '-e', applescript_command])
-subprocess.Popen(['osascript', '-e', 'tell application "Terminal" to activate'])
