c7487eb81e2f6f68500a80cbb10de9b77582bd5b
Adam Stox <adamstox@protonmail.com>
2023-04-17 00:31:53 -0400

    Update the menu to look nicer and add the stenographer guy ascii art

--- src/main.py
+++ src/main.py
@@ -20,57 +20,91 @@ class Stenographer(object):
         center_col = width // 2
         return center_row, center_col
 
+    def draw_menu_item(self, stdscr, y, x, text, is_selected):
+        text = " " + text + " "
+        box_width = len(text) + 2
+
+        if is_selected:
+            text_color = 1
+            border_characters = {'tl': '┏', 'tr': '┓', 'bl': '┗', 'br': '┛', 'h': '━', 'v': '┃'}
+        else:
+            text_color = 2
+            border_characters = {'tl': '┌', 'tr': '┐', 'bl': '└', 'br': '┘', 'h': '─', 'v': '│'}
+
+        stdscr.attron(curses.color_pair(2))
+        stdscr.addstr(y, x, border_characters['tl'] + border_characters['h'] * box_width + border_characters['tr'])
+        stdscr.addstr(y + 1, x, border_characters['v'])
+        stdscr.addstr(y + 1, x + box_width + 1, border_characters['v'])
+        stdscr.addstr(y + 2, x, border_characters['bl'] + border_characters['h'] * box_width + border_characters['br'])
+        stdscr.attroff(curses.color_pair(2))
+
+        stdscr.attron(curses.color_pair(text_color))
+        stdscr.addstr(y + 1, x + 1 + (box_width - len(text)) // 2, text)
+        stdscr.attroff(curses.color_pair(text_color))
+
     def menu(self, stdscr):
+        # Set cursor to invisible
         curses.curs_set(0)
+
+        # Enable keypad mode
         stdscr.keypad(True)
 
-        center_row, center_col = self.get_center(stdscr)
-
+        # Initialize colours
         curses.start_color()
-        curses.use_default_colors()
-        curses.init_pair(1, -1, curses.COLOR_BLACK)
-        stdscr.bkgd(curses.color_pair(1))
 
+
+        # Colours
         curses.init_color(1, 0, 0, 0)  # BLACK
         curses.init_color(2, 0, 1000, 0)  # GREEN
+
+        curses.init_pair(1, 1, 2)
         curses.init_pair(2, 2, 1)
 
-        option = 0
-        options = ["Start Recording", "Stop Recording"]
-
-        art_win = curses.newwin(9, curses.COLS, 0, 0)
-        art_win.bkgd(curses.color_pair(1))
-        art_win.attron(curses.color_pair(2))
+        # Set screen attributes
+        stdscr.bkgd(curses.color_pair(2))
         stdscr.attron(curses.color_pair(2))
 
+        # Set Title window attributes
+        title_window = curses.newwin(11, curses.COLS, 0, 0)
+        title_window.bkgd(curses.color_pair(2))
+        title_window.attron(curses.color_pair(2))
+
+        # Menu logic
+        option = 0
+        options = ["Start New Recording", "Stop Recording"]
+
         while True:
-            art_win.clear()
+            title_window.clear()
             ASCII_line_length = 143
             offset = (curses.COLS - ASCII_line_length) // 2
-            art_win.addstr(0, offset,'''                                                                                                           MMMMMMMMMMM MMMMMMMMMMMM MMMMMMMMMM ''')
-            art_win.addstr(1, offset,'''.d88888b    dP                                                                  dP                         MM'"""""`MM MM"""""""`YM M""""""""M ''')
-            art_win.addstr(2, offset,'''88.    "'   88                                                                  88                         M' .mmm. `M MM  MMMMm  M Mmmm  mmmM ''')
-            art_win.addstr(3, offset,'''`Y88888b. d8888P .d8888b. 88d888b. .d8888b. .d8888b. 88d888b. .d8888b. 88d888b. 88d888b. .d8888b. 88d888b. M  MMMMMMMM M'        .M MMMM  MMMM ''')
-            art_win.addstr(4, offset,'''      `8b   88   88ooood8 88'  `88 88'  `88 88'  `88 88'  `88 88'  `88 88'  `88 88'  `88 88ooood8 88'  `88 M  MMM   `M MM  MMMMMMMM MMMM  MMMM ''')
-            art_win.addstr(5, offset,'''d8'   .8P   88   88.  ... 88    88 88.  .88 88.  .88 88       88.  .88 88.  .88 88    88 88.  ... 88       M. `MMM' .M MM  MMMMMMMM MMMM  MMMM ''')
-            art_win.addstr(6, offset,''' Y88888P    dP   `88888P' dP    dP `88888P' `8888P88 dP       `88888P8 88Y888P' dP    dP `88888P' dP       MM.     .MM MM  MMMMMMMM MMMM  MMMM ''')
-            art_win.addstr(7, offset,'''                                                 .88                   88                                  MMMMMMMMMMM MMMMMMMMMMMM MMMMMMMMMM ''')
-            art_win.addstr(8, offset,'''                                             d8888P                    dP                                                                      ''')
+            title_window.addstr(0, offset,'''                                                                                                                 .----.''')
+            title_window.addstr(1, offset,'''.d88888b    dP                                                                  dP                              /##_##_|''')
+            title_window.addstr(2, offset,'''88.    "'   88                                                                  88                              lv(◕ |◕)''')
+            title_window.addstr(3, offset,'''`Y88888b. d8888P .d8888b. 88d888b. .d8888b. .d8888b. 88d888b. .d8888b. 88d888b. 88d888b. .d8888b. 88d888b.       l  ‿‿ j''')
+            title_window.addstr(4, offset,'''      `8b   88   88ooood8 88'  `88 88'  `88 88'  `88 88'  `88 88'  `88 88'  `88 88'  `88 88ooood8 88'  `88    _.-/\ - /\-.''')
+            title_window.addstr(5, offset,'''d8'   .8P   88   88.  ... 88    88 88.  .88 88.  .88 88       88.  .88 88.  .88 88    88 88.  ... 88         r   \_\ /_/  l''')
+            title_window.addstr(6, offset,''' Y88888P    dP   `88888P' dP    dP `88888P' `8888P88 dP       `88888P8 88Y888P' dP    dP `88888P' dP         |  `---.--.`.--.''')
+            title_window.addstr(7, offset,'''                                                 .88                   88          .-. .-. -.-               `------`\__''__' .--.''')
+            title_window.addstr(8, offset,'''                                             d8888P                    dP          \_- |-'  |                         .------'^\  \ ''')
+            title_window.addstr(9, offset,'''                                                                                                                      \_______| |  | ''')
+            title_window.addstr(10, offset,'  ')
 
             stdscr.refresh()
-            art_win.refresh()
+            title_window.refresh()
 
             separator = "━" * curses.COLS
 
-            stdscr.addstr(9, 0, separator)
-            stdscr.addstr(11, 0, "Use arrow keys to navigate, Enter to select")
-            stdscr.addstr(13, 0, "Current state: " + self.current_state)
+            stdscr.addstr(10, 0, separator)
+            stdscr.addstr(11, 5, "Use arrow keys to navigate, Enter to select")
+            stdscr.addstr(13, 5, "Current state: " + self.current_state)
 
             for i in range(len(options)):
                 if i == option:
-                    stdscr.addstr(15 + i, 0, "> " + options[i])
+                    self.draw_menu_item(stdscr, 15 + i * 4, 5, options[i], option == i)
+                    # stdscr.addstr(15 + i, 0, "> " + options[i])
                 else:
-                    stdscr.addstr(15 + i, 0, "  " + options[i])
+                    self.draw_menu_item(stdscr, 15 + i * 4, 5, options[i], option == i)
+                    # stdscr.addstr(15 + i, 0, "  " + options[i])
 
             key = stdscr.getch()
 
