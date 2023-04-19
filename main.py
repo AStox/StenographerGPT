f050033875fec84a57ae6d59b764164d7b1477fb
Adam Stox <adamstox@protonmail.com>
2023-04-18 21:27:44 -0400

    update applescript to not include rcs or profiles when loading a new terminal

--- main.py
+++ main.py
@@ -7,10 +7,16 @@ cwd = os.getcwd()
 # Define the command to run
 path = 'src/main.py'
 
+# Get the user's shell
+user_shell = os.environ.get('SHELL', '/bin/bash').split('/')[-1]
+
+# Define the appropriate shell option to skip the initialization files
+skip_rc_option = '--noprofile --norc' if user_shell == 'bash' else '--no-rcs'
+
 # Define the AppleScript command to open a new Terminal window
 applescript_command = f'''
 tell application "Terminal"
-    set newWindow to do script "python3 {os.path.join(cwd, path)}"
+    set newWindow to do script "{user_shell} {skip_rc_option} -c 'python3 {os.path.join(cwd, path)}'"
     set number of columns of newWindow to 200
     set number of rows of newWindow to 60
     activate
